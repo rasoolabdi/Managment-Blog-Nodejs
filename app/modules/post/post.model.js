6978194b53f81173b0416cbd3f93caf9472c7f2a
rasoolabdi <M0rd00r@yahoo.com>
2025-08-17 02:27:43 -0700

    added post model

---
+++ app/modules/post/post.model.js
@@ -0,0 +1,27 @@
+const { default: mongoose, model } = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const PostSchema = new mongoose.Schema({
+    title: {type: String , required: true},
+    slug: {type: String , required: true , unique: true},
+    category: {type: ObjectId , ref: "category" , required: true},
+    type: {type: String , required: true , default: "free" , enum: ["free" , "premium"]},
+    briefText: {type: String , required: true},
+    text: {type: String , required: true},
+    coverImage: {type: String , required: true , unique: true},
+    likes: {type: [ObjectId] , ref: "user"},
+    bookmarks: {type: [ObjectId] , ref: "user"},
+    readingTime: {type: Number , required: true},
+    tags: {type: [String] , required: false},
+    author: {type: ObjectId , ref: "user"},
+    related: {type: [ObjectId] , ref: "post"},
+    comments: {type: [ObjectId] , ref: "comment"}
+} , {
+    timestamps: true,
+    toJSON: {
+        virtuals: true
+    }
+});
+
+const PostModel = model("post" , PostSchema);
+module.exports = PostModel;
